4ca3248fd683bdef407f5c1d8ac89841f795cd8b
suyeon-ham-01 <hammun6@kookmin.ac.kr>
2020-06-27 16:57:44 +0900

    issue #4 add calc diretory and wsgl:두 수의 합과 곱 codes

---
+++ calc/sum.py
@@ -0,0 +1,24 @@
+from cgi import parse_qs
+from template import html
+
+def application(environ, start_response):
+	d = parse_qs(environ['QUERY_STRING'])
+	x = d.get('x', [''])[0]
+	y = d.get('y', [''])[0]
+	sum = 0
+	mul = 0
+	if '' not in [x, y]:
+		x, y = int(x), int(y)
+
+		sum = x + y
+		mul = x * y 
+		
+	response_body = html % {
+		'sum' : sum,
+		'mul' : mul,
+	}
+	start_response('200 OK', [
+		('Content-Type', 'text/html'),
+		('Content-Length', str(len(response_body)))
+	])
+	return [response_body]
---
+++ calc/template.py
@@ -0,0 +1,21 @@
+html = """
+<html>
+	<body>
+		<form method="get" action="">
+			<p>
+			x = <input type="number" name="x">
+			</p>
+			<p>
+			y = <input type="number" name="y">
+			</p>
+			<p>
+			<input type="submit">
+			</p>
+		</form>
+		<p>
+		SUM : %(sum)s</br>
+		MUL : %(mul)s</br>
+		</p>
+	</body>
+</html>
+"""
